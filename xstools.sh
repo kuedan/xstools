2b5a590979c02558bcc289bbb61ecba02bb2ea90
itsme <itsme.xonotic@ymail.com>
2012-03-23 17:02:44 +0100

    fix

--- xstools.sh
+++ xstools.sh
@@ -124,7 +124,7 @@ function version_git_check_and_set() {
 
 function update_git() {
 cd "$basedir_git"
-echo "// this file defines the last update date of your Xonotic git 
+echo "// this file defines the last update date of your Xonotic git repo 
 // everytime you run an update the date of the builddate-git variable changes
 // you can define the date format in configs/xstools.conf
 set builddate-git \"$(date +"$git_update_date")\"" > "$userdir/configs/servers/common/builddate-git.cfg" &&
